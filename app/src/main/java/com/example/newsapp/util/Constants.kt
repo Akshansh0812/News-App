7d26552ac29fbb0285ab4fad3b4a0ff0c3860e88
Akshansh0812 <akshanshanand2001@gmail.com>
2023-09-09 16:47:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "47be95b74d364988935f8f655f51c80f"
+        const val BASE_URL = "http://newsapi.org"
+    }
+}
